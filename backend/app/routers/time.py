0551ec093387139470807dac00d305b328d1f42d
Yuta Yamaoka <yamaokayuta2000@gmail.com>
2024-05-31 23:54:42 +0900

    データが未登録時の例外処理を追加

--- backend/app/routers/time.py
+++ backend/app/routers/time.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from app.models.time_model import TimeIn, RegisterSalary
 from datetime import datetime
 import pytz
@@ -7,7 +7,7 @@ router = APIRouter()
 
 now = format(datetime.now(pytz.timezone('Asia/Tokyo')), "%Y-%m-%d")
 today_situation = {"target": "未登録", "study": "未登録", "date": now}
-current_salary = {"base": 0, "bonus": 0}
+current_salary = {"base": "未登録", "bonus": 0}
 
 
 @router.get("/today", status_code=200)
@@ -19,6 +19,8 @@ async def show_today_situation():
         return {"today situation": today_situation,
                 "present achivement": current_salary}
     else:
+        if current_salary["base"] == "未登録":
+            raise HTTPException(status_code=400, detail="月収を登録して下さい")
         added_salary = current_salary["base"].salary + current_salary["bonus"]
         return {
             "today situation": today_situation,
@@ -35,12 +37,18 @@ async def register_today_target(hour: TimeIn):
 
 @router.post("/study_time", status_code=201, response_model=TimeIn)
 async def register_study_time(hour: TimeIn):
+    if today_situation["target"] == "未登録":
+        raise HTTPException(status_code=400, detail="先に本日の目標を入力して下さい")
     today_situation["study"] = hour
     return hour
 
 
 @router.get("/finish_today", status_code=200)
 async def finish_today_work():
+    if today_situation["target"] == "未登録":
+        raise HTTPException(status_code=400, detail="本日の目標を入力して下さい")
+    elif today_situation["study"] == "未登録":
+        raise HTTPException(status_code=400, detail="本日の勉強時間を入力して下さい")
     target = today_situation["target"].hour
     achievement = today_situation["study"].hour
     if achievement >= target:
