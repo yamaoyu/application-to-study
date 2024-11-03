5246e6878a580aa61e5ecc4cf3809e7fbe9b3366
Yuta Yamaoka <yamaokayuta2000@gmail.com>
2024-11-03 23:45:58 +0900

    alembicを使用するための初期設定

--- backend/settings.sh
+++ backend/settings.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+if [ ! -d "alembic" ]; then
+    alembic init alembic
+    echo "alembic initialized"
+else
+    echo "alembic already initialized"
+
+fi
+
 python settings.py
 
 uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
