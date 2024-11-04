6bb9e3267f87d0ab02a7199943a76914ca06d6cf
Yuta Yamaoka <yamaokayuta2000@gmail.com>
2024-11-04 12:46:55 +0900

    alembicにて対象のDBを環境変数を使用して設定できるよう改修

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,3 +1,4 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -8,6 +9,8 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_section_option("alembic", "DB_URL", os.getenv("DB_URL"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
