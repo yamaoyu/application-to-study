8b394e39c9f3762dfe404b99e4dd53779d1010c4
Yuta Yamaoka <yamaokayuta2000@gmail.com>
2024-11-06 23:59:34 +0900

    収入テーブルの名称変更に伴う修正(alembic使用)

---
+++ backend/alembic/versions/給料テーブルの名称の変更_2024-11-06_89511bb8c5d8.py
@@ -0,0 +1,31 @@
+"""給料テーブルの名称の変更
+
+Revision ID: 89511bb8c5d8
+Revises: 
+Create Date: 2024-11-06 23:25:52.395801+09:00
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy import Float, Integer
+
+
+# revision identifiers, used by Alembic.
+revision: str = '89511bb8c5d8'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.rename_table(old_table_name="earnings", new_table_name="incomes")
+    op.alter_column(table_name='incomes', column_name='monthly_income',
+                    new_column_name='salary', existing_type=Float(4, 1))
+
+
+def downgrade() -> None:
+    op.rename_table(old_table_name="incomes", new_table_name="earnings")
+    op.alter_column(table_name='earnings', column_name='salary',
+                    new_column_name='monthly_income', existing_type=Float(4, 1))
