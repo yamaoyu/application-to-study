1bc25f89dd47cc8fe02d9bb4e80a7fec5cbc0944
Yuta Yamaoka <yamaokayuta2000@gmail.com>
2025-01-25 14:47:12 +0900

    #77 認証処理のリファクタリング

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -12,31 +12,41 @@ createApp(App).use(router).use(pinia).mount('#app')
 
 // トークンが無効、もしくはない場合はログインページとユーザー登録ページ以外は開けないようにする
 const authStore = useAuthStore()
+
+const allowedRoutes = ['Login', 'RegisterUser']
+
+const verfiyRefreshToken = async () => {
+    // デバイス情報を取得 HTTPSにするまでの一時的な対応としてplatform(非推奨)を使用
+  const device = navigator.platform || "unknown";
+  const response = await axios.post(
+    process.env.VUE_APP_BACKEND_URL + "token",
+    { device: device },
+    { withCredentials: true })
+  return response
+}
+
 router.beforeEach(async (to) => {
   // 遷移先がログインページとユーザー登録ページ以外の場合
-  if (to.name !== 'Login' && to.name !== 'RegisterUser') {
-    // トークンがない、もしくは期限切れの場合
-    if (!authStore.isToken || authStore.isExpired()) {
-      // リフレッシュトークンの検証
-      try{
-        // デバイス情報を取得 HTTPSにするまでの一時的な対応としてplatform(非推奨)を使用
-        const device = navigator.platform || "unknown";
-        const response = await axios.post(
-          process.env.VUE_APP_BACKEND_URL + "token", 
-          { device: device },
-          { withCredentials: true })
-        if (response.status === 200) {
-          // トークンの更新
-          await authStore.setAuthData(
-            response.data.access_token,
-            response.data.token_type,
-            jwtDecode(response.data.access_token).exp)
-        } else {
-          return { name: 'Login' }
-        }
-      } catch(error){
+  if (allowedRoutes.includes(to.name)) {
+    return
+  }
+  // トークンがない、もしくは期限切れの場合
+  if (!authStore.isToken || authStore.isExpired()) {
+    // リフレッシュトークンの検証
+    try{
+      const response = await verfiyRefreshToken()
+      if (response.status === 200) {
+        // トークンの更新
+        await authStore.setAuthData(
+          response.data.access_token,
+          response.data.token_type,
+          jwtDecode(response.data.access_token).exp)
+      } else {
         return { name: 'Login' }
       }
+    } catch(error){
+      return { name: 'Login' }
     }
   }
-})
+}
+)
