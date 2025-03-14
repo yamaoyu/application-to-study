d0fe47ee9511a144e2c12a97a60b3830d5400ea4
Yuta Yamaoka <yamaokayuta2000@gmail.com>
2025-03-14 23:00:38 +0900

    #84 ユーザー登録時2回入力にする

--- frontend/src/views/RegisterUser.vue
+++ frontend/src/views/RegisterUser.vue
@@ -1,22 +1,38 @@
 <template>
+  <nav class="navbar navbar-expand-lg bd-navbar fixed-top bg-dark navbar-dark">
+    <div class="container-fluid">
+      <ul class="navbar-nav ms-auto me-2" style="color: white;">
+        <li class="nav-item">
+          <router-link class="nav-link fs-5 py-1" to="/login">LOGIN</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
   <h2>ユーザー登録</h2>
   <BForm @submit.prevent="createUser" class="container d-flex flex-column align-items-center">
     <div class="form-group mt-3 col-8">
-      <BFormInput placeholder="username(必須)" v-model="username" :state="validateUsername" required/>
+      <BFormInput placeholder="ユーザー名(必須)" v-model="username" :state="validateUsername" required/>
       <BFormInvalidFeedback :state="validateUsername">
         ユーザー名は3文字以上16文字以下にして下さい
       </BFormInvalidFeedback>
       <BFormValidFeedback :state="validateUsername"> OK </BFormValidFeedback>
     </div>
     <div class="form-group mt-3 col-8">
-      <BFormInput type="password" placeholder="password(必須)" v-model="password" :state="validatePassword" required/>
+      <BFormInput type="password" placeholder="パスワード(必須)" v-model="password" :state="validatePassword" required/>
       <BFormInvalidFeedback :state="validatePassword">
         パスワードは8文字以上16文字以下にして下さい
       </BFormInvalidFeedback>
       <BFormValidFeedback :state="validatePassword"> OK </BFormValidFeedback>
     </div>
     <div class="form-group mt-3 col-8">
-      <BFormInput placeholder="email(任意)" type="email" v-model="email" :state="validateEmail" />
+      <BFormInput type="password" placeholder="パスワード確認(必須)" v-model="passwordCheck" :state="checkPassword" required/>
+      <BFormInvalidFeedback :state="checkPassword">
+        パスワードが一致しません
+      </BFormInvalidFeedback>
+      <BFormValidFeedback :state="checkPassword"> OK </BFormValidFeedback>
+    </div>
+    <div class="form-group mt-3 col-8">
+      <BFormInput placeholder="メールアドレス(任意)" type="email" v-model="email" :state="validateEmail" />
       <BFormInvalidFeedback :state="validateEmail">
         メールアドレスの形式で入力して下さい
       </BFormInvalidFeedback>
@@ -49,23 +65,34 @@
     setup() {
       const username = ref('')
       const password = ref('')
+      const passwordCheck = ref('')
       const email = ref('')
       const message = ref('')
       const statusCode = ref()
 
       const validateUsername = computed(() => {
+        // ユーザー名が3文字以上16文字以下かどうかを判定
         if (username.value.length === 0) {
           return null
         }
         return username.value.length >= 3 && username.value.length <= 16})
 
       const validatePassword = computed(() => {
+        // パスワードが8文字以上16文字以下かどうかを判定
         if (password.value.length === 0) {
           return null
         }
         return password.value.length >= 8 && password.value.length <= 16})
 
+      const checkPassword = computed(() => {
+        // 入力された2つパスワードが一致しているかどうかを判定
+        if (passwordCheck.value.length === 0) {
+          return null
+        }
+        return password.value === passwordCheck.value})
+
       const validateEmail = computed(() => {
+        // メールアドレスの形式かどうかを判定
         if (email.value.length === 0) {
           return null
         }
@@ -74,6 +101,13 @@
       })
   
       const createUser = async() => {
+        // パスワード不一致の場合はリクエストを送信しない
+        if (password.value !== passwordCheck.value) {
+          statusCode.value = null;
+          message.value = "パスワードが一致しません\nパスワードを確認してください";
+          return;
+        }
+
         try {
           const url = process.env.VUE_APP_BACKEND_URL + 'users'
           const response = await axios.post(
@@ -111,6 +145,7 @@
       return {
         username,
         password,
+        passwordCheck,
         email,
         message,
         statusCode,
@@ -118,6 +153,7 @@
         getResponseAlert,
         validateUsername,
         validatePassword,
+        checkPassword,
         validateEmail
       }
     }
