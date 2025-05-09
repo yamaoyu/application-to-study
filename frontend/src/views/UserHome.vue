38d811f2a4ed153f3100b45ff08834bec5d00d71
Yuta Yamaoka <yamaokayuta2000@gmail.com>
2025-05-10 03:00:29 +0900

    #121 todoの表示順のソート

--- frontend/src/views/UserHome.vue
+++ frontend/src/views/UserHome.vue
@@ -91,7 +91,19 @@
     </div>
   </div>
   <div class="container">
-    <h2>Todo一覧</h2>
+    <div class="position-relative mb-3">
+      <!-- 中央に配置するためのコンテナ -->
+      <h2 class="text-center">Todo一覧</h2>
+      <!-- 右側に絶対配置でボタンを配置 -->
+      <div class="btn-group position-absolute top-50 end-0 translate-middle-y">
+        <BButton class="btn btn-outline-secondary bi-sort-down btn-sm" :variant="sortType === 'id' ? 'secondary text-white' : 'outline-secondary'" @click="sortTodos('id')">
+          登録順
+        </BButton>
+        <BButton class="btn btn-outline-secondary bi-sort-down btn-sm" :variant="sortType === 'due' ? 'secondary text-white' : 'outline-secondary'" @click="sortTodos('due')">
+          期限順
+        </BButton>
+      </div>
+    </div>
     <template v-if="todos.length">
       <table class="table table-striped table-responsive">
         <thead class="table-dark">
@@ -124,7 +136,7 @@
   </div>
 
   <!-- モーダルコンポーネントで登録前の確認 -->
-  <BModal v-model="showModal" :title="modalTitle" ok-title="はい" cancel-title="いいえ" @ok="sendRequest">
+  <BModal v-model="showModal" :title="modalTitle" ok-title="はい" cancel-title="いいえ" @ok="sendTodoRequest">
     <p class="text-danger">確定後は取り消せません</p>
   </BModal>
 
@@ -139,11 +151,12 @@ import { useAuthStore } from '@/store/authenticate';
 import { useTodoStore } from '@/store/todo';
 import { jwtDecode } from 'jwt-decode';
 import { STATUS_DICT, getAdjustmentColors, getStatusColors, getActivityAlert, commonError, verifyRefreshToken } from './lib';
-import { BModal } from 'bootstrap-vue-next';
+import { BButton, BModal } from 'bootstrap-vue-next';
 
 export default {
   components:{
-    BModal
+    BModal,
+    BButton
   },
 
   setup() {
@@ -162,6 +175,7 @@ export default {
     const modalTitle = ref("")
     const todoId = ref()
     const todoAction = ref()
+    const sortType = ref("id")
     const router = useRouter()
     const authStore = useAuthStore()
     const todoStore = useTodoStore()
@@ -178,7 +192,7 @@ export default {
         }
     }
 
-    const sendRequest = async() =>{
+    const sendTodoRequest = async() =>{
       if (todoAction.value==='finish'){
         await finishTodo(todoId.value)
       } else if (todoAction.value==='delete') {
@@ -187,6 +201,19 @@ export default {
       // データを初期化
       todoId.value = null
       todoAction.value = null
+    }
+
+    const sortTodos = async(type) =>{
+      sortType.value = type
+      if (sortType.value==="id"){
+        todos.value.sort((item1, item2) => item1.todo_id - item2.todo_id);
+      } else {
+        todos.value.sort((item1, item2) => {
+          if (item1.due > item2.due) return 1;
+          if (item1.due < item2.due) return -1;
+          return 0;
+        });
+      }
     }
 
     const updateTodos = async() =>{
@@ -424,7 +451,9 @@ export default {
       month,
       date,
       confirmRequest,
-      sendRequest,
+      sendTodoRequest,
+      sortTodos,
+      sortType,
       updateTodos,
       deleteTodo,
       finishTodo,
