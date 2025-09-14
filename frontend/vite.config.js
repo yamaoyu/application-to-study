1575a6d608a5c8ca6d7f9d399a1672a4c02777b9
Yuta Yamaoka <yamaokayuta2000@gmail.com>
2025-09-14 18:19:17 +0900

    #130 viteへの移行(環境変数名を修正)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,12 +13,12 @@ export default defineConfig({
         setupFiles: ['./test/vitest.setup.js'],
     },
     server: {
-        port: process.env.VITE_FRONTEND_PORT,
+        port: process.env.FRONTEND_PORT,
         host: "0.0.0.0",
-        allowedHosts: [process.env.VITE_FRONTEND_DOMAIN],
+        allowedHosts: [process.env.FRONTEND_DOMAIN],
         hmr: {
             port: 24678,                             // HMR用WebSocket公開ポート
-            host: process.env.VITE_FRONTEND_DOMAIN,  // ブラウザから見えるホスト名
+            host: process.env.FRONTEND_DOMAIN,  // ブラウザから見えるホスト名
             clientPort: 443,
             protocol: 'wss',
         }
