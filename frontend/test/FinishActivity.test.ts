fb00162c2ab1409974dd2b0288b9fd7c905eed97
Yuta Yamaoka <yamaokayuta2000@gmail.com>
2026-07-16 23:05:33 +0900

    refactor #212 活動登録関連のテスト修正(フロントエンド)

--- frontend/test/FinishActivity.test.ts
+++ frontend/test/FinishActivity.test.ts
@@ -146,7 +146,8 @@ describe('活動の終了(一括)', () => {
         results: [
           { "date": "2025/1/1", "result": "error", "reason": "ACTIVITY_NOT_FOUND" },
           { "date": "2025/1/2", "result": "error", "reason": "SALARY_NOT_FOUND" },
-          { "date": "2025/1/3", "result": "error", "reason": "UNEXPECTED_ERROR" }
+          { "date": "2025/1/3", "result": "error", "reason": "UNEXPECTED_ERROR" },
+          { "date": "2025/1/4", "result": "error", "reason": "ACTIVITY_ALREADY_FINISHED" }
         ]
       }
     });
@@ -168,7 +169,7 @@ describe('活動の終了(一括)', () => {
         dates: expectedDates
       }
     );
-    const expectedMessage = "2025/1/1の活動終了に失敗: 目標時間が未登録です\n2025/1/2の活動終了に失敗: 月収が未登録です\n2025/1/3の活動終了に失敗: 予期せぬエラーが発生しました";
+    const expectedMessage = "2025/1/1の活動終了に失敗: 目標時間が未登録です\n2025/1/2の活動終了に失敗: 月収が未登録です\n2025/1/3の活動終了に失敗: 予期せぬエラーが発生しました\n2025/1/4の活動終了に失敗: 既に確定されています";
     expect(wrapper.find("[data-testid='reqMsg']").text()).toEqual(expectedMessage);
   })
 });
